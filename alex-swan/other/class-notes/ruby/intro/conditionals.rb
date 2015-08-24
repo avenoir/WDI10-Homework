12b295d102575ae50c7c84078e9a550909b98a44
ALEXANDRA SWAN <alexandraswan@gmail.com>
2015-08-24 12:35:56 +1000

    intro to ruby

---
+++ alex-swan/other/class-notes/ruby/intro/conditionals.rb
@@ -0,0 +1,37 @@
+# Examples of contitionals
+
+if 7 > 3
+	puts "Mathematics is behaving in Ruby"
+end
+
+# Terrible offensie example
+gender = 'i don\'t know'
+if gender == 'male'
+	puts "You are a male"
+else
+	puts "You must be female"
+end
+
+# Better example
+if gender == 'male'
+	puts "You are a male"
+elsif gender == 'female'
+	puts "You are a female"
+else 
+	puts "Gender indeterminate"
+end
+
+# Can do if statements on one line
+puts "23 is greater than 4" if 23 > 4	
+
+
+bananas = false
+if bananas !=true
+	puts "You need to buy bananas"
+end
+	
+unless bananas == true
+	puts "You need to buy bananas"
+end
+
+puts "You need to buy bananas" unless bananas
