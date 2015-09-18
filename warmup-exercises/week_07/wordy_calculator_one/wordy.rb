807ded99f83aa6e2f6ea79c48d44ebdc77921985
anonymous-wolf <jackjeffress@gmail.com>
2015-09-18 10:35:22 +1000

    Added warmup

--- warmup-exercises/week_07/wordy_calculator_one/wordy.rb
+++ warmup-exercises/week_07/wordy_calculator_one/wordy.rb
@@ -1,43 +1,6 @@
-require 'pry'
 class Calculator
-  attr_reader :question
-  def initialize question
-    @question = question
-    ask
-  end
-
-  def ask
-    matches = @question.match(  /(?<first-num>-?\d+)/  )
-    binding.pry
-    puts ( matches[1].to_i + matches[2].to_i )
+  def ask(question)
+    matches = question.match(/What is (-?\d+) plus (-?\d+)?/)
     matches[1].to_i + matches[2].to_i
   end
 end
-
-calc = Calculator.new "What is -55 plus 5?"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Calculator
-#   def ask(question)
-#     matches = question.match(/What is (-?\d+) plus (-?\d+)?/)
-#     matches[1].to_i + matches[2].to_i
-#   end
-# end
